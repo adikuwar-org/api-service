31feeec88f1275841c24bc092c0f372a3b9bca2a
adikuwar37 <adikuwar37@outlook.com>
2022-08-06 19:24:37 +0530

    Refractor logic of Series controller to Series Service #16

--- src/series/series.controller.ts
+++ src/series/series.controller.ts
@@ -10,39 +10,51 @@ import {
   HttpStatus,
   HttpException,
   HttpCode,
+  NotFoundException,
 } from '@nestjs/common';
-import { SeriesService } from './series.service';
+import { SeriesService, SeriesErrors } from './series.service';
 import { CreateSeriesDto } from './dto/create-series.dto';
 import { UpdateSeriesDto } from './dto/update-series.dto';
 import { SeriesResponseDto } from './dto/series-response.dto';
 
+class SeriesNameUniquenessException extends HttpException {
+  constructor(seriesName) {
+    super(
+      `Series with name '${seriesName}' already exist`,
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+}
+
+class SeriesInvalidIdException extends HttpException {
+  constructor(id) {
+    super(`Series id '${id}' is invalid`, HttpStatus.BAD_REQUEST);
+  }
+}
+
 @Controller('series')
 export class SeriesController {
   private readonly logger = new Logger(SeriesController.name);
+
   constructor(private readonly seriesService: SeriesService) {}
 
   @Post()
   async create(@Body() createSeriesDto: CreateSeriesDto) {
-    // Verify name uniqueness
-    const seriesName: string = createSeriesDto.name;
-    this.logger.debug(
-      `Verifying series with name : ${seriesName} do not exsit`,
-    );
-    const series = await this.seriesService.findOneWithName(seriesName);
-
-    if (series) {
-      this.logger.error(`Series with name : ${seriesName} already exist`);
-      throw new HttpException(
-        `Series with name '${seriesName}' already exist`,
-        HttpStatus.BAD_REQUEST,
-      );
-    } else {
-      this.logger.debug(`Series name : ${seriesName} is unique`);
-    }
-
     this.logger.log('Creating series');
     this.logger.verbose('Creating series : ', createSeriesDto);
-    const createdSeries = await this.seriesService.create(createSeriesDto);
+
+    let createdSeries = null;
+    try {
+      createdSeries = await this.seriesService.create(createSeriesDto);
+    } catch (error) {
+      this.logger.error('Failed to create series', error);
+      if (error.message === SeriesErrors.NameUniquenessError) {
+        throw new SeriesNameUniquenessException(createSeriesDto.name);
+      } else {
+        throw error;
+      }
+    }
+
     this.logger.debug(`Series created with id : ${createdSeries.id}`);
     this.logger.verbose(`Created series`, createdSeries.toObject());
     return new SeriesResponseDto(createdSeries);
@@ -59,14 +71,27 @@ export class SeriesController {
   @Get(':id')
   async findOne(@Param('id') id: string) {
     this.logger.debug(`Fetching series with id : ${id}`);
-    const series = await this.seriesService.findOne(id);
+
+    let series;
+
+    try {
+      series = await this.seriesService.findOne(id);
+    } catch (error) {
+      this.logger.error(`Failed to fatch series with id : ${id}`, error);
+      switch (error.message) {
+        case SeriesErrors.InvalidObjectId:
+          throw new SeriesInvalidIdException(id);
+        default:
+          throw error;
+      }
+    }
 
     // check if series exist
     if (series) {
       this.logger.debug(`Fetched series with id : ${id}`);
       return new SeriesResponseDto(series);
     } else {
-      throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+      throw new NotFoundException();
     }
   }
 
@@ -76,39 +101,33 @@ export class SeriesController {
     @Body() updateSeriesDto: UpdateSeriesDto,
   ) {
     this.logger.debug(`Updating series with id : ${id}`);
-
-    // if name is to be updated verify that series with given name do not exist
-    const seriesName = updateSeriesDto.name;
-    if (seriesName) {
-      this.logger.debug(`Verifying series name '${seriesName} is unique`);
-      const series = await this.seriesService.findOneWithName(seriesName);
-      if (series && id !== series.id) {
-        // series with given name already exist
-        this.logger.error(`Series with name : ${seriesName} already exist`);
-        throw new HttpException(
-          `Series with name '${seriesName}' already exist`,
-          HttpStatus.BAD_REQUEST,
-        );
-      } else {
-        this.logger.debug(`Series name '$SeriesName} is unique`);
-      }
-    } else {
-      this.logger.log(`Series name is not provided for update`);
-    }
-
     this.logger.verbose(
       `Updating series with id : ${id} with data`,
       updateSeriesDto,
     );
-    await this.seriesService.update(id, updateSeriesDto);
+
+    let updatedSeries;
+    try {
+      updatedSeries = await this.seriesService.update(id, updateSeriesDto);
+    } catch (error) {
+      this.logger.error(
+        'Failed to update series id : ${id} with data',
+        updateSeriesDto,
+      );
+      this.logger.error('Due to error', error);
+      switch (error.message) {
+        case SeriesErrors.NameUniquenessError:
+          throw new SeriesNameUniquenessException(updateSeriesDto.name);
+        case SeriesErrors.NotFound:
+          throw new NotFoundException();
+        case SeriesErrors.InvalidObjectId:
+          throw new SeriesInvalidIdException(id);
+        default:
+          throw error;
+      }
+    }
     this.logger.debug(`Updated series with id : ${id}`);
     this.logger.debug(`Fetching updated series with id : ${id}`);
-    const updatedSeries = await this.seriesService.findOne(id);
-    this.logger.debug(`Fetched updated series with id : ${id}`);
-    this.logger.verbose(
-      `Updated series with id : ${id}`,
-      updatedSeries.toObject(),
-    );
     return new SeriesResponseDto(updatedSeries);
   }
 
@@ -116,11 +135,25 @@ export class SeriesController {
   @HttpCode(HttpStatus.NO_CONTENT)
   async remove(@Param('id') id: string): Promise<void> {
     this.logger.debug(`Deleting series with id : ${id}`);
-    const deletedSeries = await this.seriesService.remove(id);
+    let deletedSeries;
+
+    try {
+      deletedSeries = await this.seriesService.remove(id);
+    } catch (error) {
+      this.logger.error(`Failed to delete series`, error);
+      switch (error.message) {
+        case SeriesErrors.InvalidObjectId:
+          throw new SeriesInvalidIdException(id);
+        default:
+          throw error;
+      }
+    }
+
     if (deletedSeries) {
       this.logger.debug(`Deleted series with id : ${id}`);
     } else {
-      this.logger.debug(`Series with id : $${id} do not exist`);
+      this.logger.error(`Series with id : ${id} do not exist`);
+      throw new NotFoundException();
     }
   }
 }
