81bee6a6450d1e4f3c5fe5fdd1d6c028cd27493a
adikuwar37 <adikuwar37@outlook.com>
2022-08-16 11:06:01 +0530

    #23: adds ut for series.service.findAll()

--- src/series/series.service.spec.ts
+++ src/series/series.service.spec.ts
@@ -1,23 +1,41 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Model } from 'mongoose';
+import { Series } from './schemas/series.schema';
 import { SeriesService } from './series.service';
 
 describe('SeriesService', () => {
   let service: SeriesService;
+  let model: Model<Series>;
+
+  const seriesArray = [];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
+        SeriesService,
         {
-          provide: SeriesService,
-          useValue: {},
+          provide: getModelToken('Series'),
+          useValue: {
+            find: jest.fn(),
+          },
         },
       ],
     }).compile();
 
     service = module.get<SeriesService>(SeriesService);
+    model = module.get<Model<Series>>(getModelToken('Series'));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return all series', async () => {
+    jest.spyOn(model, 'find').mockReturnValue({
+      exec: jest.fn().mockResolvedValueOnce(seriesArray),
+    } as any);
+    const series = await service.findAll();
+    expect(series).toEqual(seriesArray);
   });
 });
