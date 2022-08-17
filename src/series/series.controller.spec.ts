c68af6231d5cf7a0dbaa919080caaaab7a27c4fa
adikuwar37 <adikuwar37@outlook.com>
2022-08-17 10:26:51 +0530

    #23: adds unit tests for SeriesController
    - adds unit tests for create() and findAll()

--- src/series/series.controller.spec.ts
+++ src/series/series.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SeriesController } from './series.controller';
-import { SeriesService } from './series.service';
+import { SeriesService, SeriesErrors } from './series.service';
 
 describe('SeriesController', () => {
   let controller: SeriesController;
@@ -11,7 +11,37 @@ describe('SeriesController', () => {
       providers: [
         {
           provide: SeriesService,
-          useValue: {},
+          useValue: {
+            create: jest.fn().mockImplementation((series: any) => {
+              if (series.name === 'NotUnique') {
+                throw new Error(SeriesErrors.NameUniquenessError);
+              }
+              return Promise.resolve({
+                name: series.name,
+                _id: 'id',
+                toObject: () => {
+                  return {
+                    name: series.name,
+                    _id: 'id',
+                  };
+                },
+              });
+            }),
+            findAll: jest.fn().mockResolvedValue([
+              {
+                name: 'Series 1',
+                _id: 'id1',
+              },
+              {
+                name: 'Series 2',
+                _id: 'id2',
+              },
+              {
+                name: 'Series 3',
+                _id: 'id3',
+              },
+            ]),
+          },
         },
       ],
     }).compile();
@@ -21,5 +51,51 @@ describe('SeriesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('SeriesController.create', () => {
+    it('should create series', async () => {
+      const createdSeries = await controller.create({
+        name: 'Series 1',
+      });
+
+      const expectedSeries = {
+        name: 'Series 1',
+        id: 'id',
+      };
+
+      expect(createdSeries).toEqual(expectedSeries);
+    });
+
+    it('should throw error if name is not unique', async () => {
+      expect(
+        controller.create({
+          name: 'NotUnique',
+        }),
+      ).rejects.toThrow(`Series with name 'NotUnique' already exist`);
+    });
+  });
+
+  describe('SeriesController.findAll', () => {
+    it('should return all series', async () => {
+      const seriesList = await controller.findAll();
+
+      const expectedSeriesList = [
+        {
+          name: 'Series 1',
+          id: 'id1',
+        },
+        {
+          name: 'Series 2',
+          id: 'id2',
+        },
+        {
+          name: 'Series 3',
+          id: 'id3',
+        },
+      ];
+
+      expect(seriesList).toEqual(expectedSeriesList);
+    });
   });
 });
