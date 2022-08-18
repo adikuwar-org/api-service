735944db90b221660fa5d6024e0f15b99988d27a
adikuwar37 <30593308+AdiKuwar37@users.noreply.github.com>
2022-08-18 10:34:07 +0530

    #23: add unit tests for SeriesController
    - adds unit tests for update()
    - adds unit tests for remove()
    - adds jest.config.ts. adds codeCoverageThreshold

---
+++ jest.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from '@jest/types';
+
+// Sync object
+const config: Config.InitialOptions = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/src/main.ts',
+    '/src/app.module.ts',
+    '/src/series/series.module.ts',
+  ],
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 85,
+      functions: 90,
+      lines: 90,
+    },
+  },
+};
+export default config;
--- src/series/series.controller.spec.ts
+++ src/series/series.controller.spec.ts
@@ -58,6 +58,55 @@ describe('SeriesController', () => {
                 return Promise.resolve(existingSeries[index]);
               }
             }),
+            update: jest.fn().mockImplementation((id: string, update: any) => {
+              if (!mongoose.Types.ObjectId.isValid(id)) {
+                throw new Error(SeriesErrors.InvalidObjectId);
+              }
+
+              const index = existingSeries.findIndex(
+                (series) => series._id === id,
+              );
+
+              if (index < 0) {
+                throw new Error(SeriesErrors.NotFound);
+              } else {
+                let existingSeriesWithNameIndex = -1;
+                if (update.name) {
+                  existingSeriesWithNameIndex = existingSeries.findIndex(
+                    (series) =>
+                      series._id !== id && series.name === update.name,
+                  );
+                }
+
+                if (existingSeriesWithNameIndex < 0) {
+                  const updatedSeries: any = {
+                    _id: id,
+                  };
+                  if (update.name) {
+                    updatedSeries.name = update.name;
+                  } else {
+                    updatedSeries.name = existingSeries[index].name;
+                  }
+                  return Promise.resolve(updatedSeries);
+                } else {
+                  throw new Error(SeriesErrors.NameUniquenessError);
+                }
+              }
+            }),
+            remove: jest.fn().mockImplementation((id: string) => {
+              if (mongoose.Types.ObjectId.isValid(id)) {
+                const index = existingSeries.findIndex(
+                  (series) => series._id === id,
+                );
+                if (index < 0) {
+                  return Promise.resolve(null);
+                } else {
+                  return Promise.resolve(existingSeries[index]);
+                }
+              } else {
+                throw new Error(SeriesErrors.InvalidObjectId);
+              }
+            }),
           },
         },
       ],
@@ -139,5 +188,66 @@ describe('SeriesController', () => {
     return expect(
       controller.findOne('62ee91648e835835481d53fd'),
     ).rejects.toThrow('Not Found');
+  });
+
+  describe('SeriesController.update', () => {
+    it('should update series', async () => {
+      const updatedSeries = await controller.update(existingSeries[0]._id, {
+        name: 'Series 4',
+      });
+
+      const expectedSeries = {
+        name: 'Series 4',
+        id: existingSeries[0]._id,
+      };
+
+      expect(updatedSeries).toEqual(expectedSeries);
+    });
+
+    it('should throw error if another series with name already exist', async () => {
+      return expect(
+        controller.update(existingSeries[0]._id, {
+          name: existingSeries[1].name,
+        }),
+      ).rejects.toThrow(
+        `Series with name '${existingSeries[1].name}' already exist`,
+      );
+    });
+
+    it('should throw error if series is not found', async () => {
+      return expect(
+        controller.update('62ee91648e835835481d53fd', {
+          name: 'Series 4',
+        }),
+      ).rejects.toThrow('Not Found');
+    });
+
+    it('should throw error if id is invalid', async () => {
+      return expect(
+        controller.update('invalid', {
+          name: 'Series 4',
+        }),
+      ).rejects.toThrow(`Series id 'invalid' is invalid`);
+    });
+  });
+
+  describe('SeriesService.remove', () => {
+    it('should delete series', async () => {
+      return expect(
+        controller.remove(existingSeries[0]._id),
+      ).resolves.toBeUndefined();
+    });
+
+    it('should throw error when id is invalid', async () => {
+      return expect(controller.remove(`invalid`)).rejects.toThrow(
+        `Series id 'invalid' is invalid`,
+      );
+    });
+
+    it('should throw error when series with id does not exist', async () => {
+      return expect(
+        controller.remove('62ee91648e835835481d53fd'),
+      ).rejects.toThrow('Not Found');
+    });
   });
 });
