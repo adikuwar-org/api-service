6c322fa2e8478286a94f2ed4a0c68be4f049a486
AdiKuwar37 <adikuwar37@outlook.com>
2022-10-28 10:27:57 +0530

    #28: adds uts for user policies

--- src/users/policies/create-user-policy.handler.spec.ts
+++ src/users/policies/create-user-policy.handler.spec.ts
@@ -1,7 +1,50 @@
+import { CaslAbilityFactory } from 'src/casl/casl-ability.factory';
+import { Roles } from '../schemas/users.schema';
 import { CreateUserPolicyHandler } from './create-user-policy.handler';
 
 describe('CreateUserPolicyHandler', () => {
   it('should be defined', () => {
     expect(new CreateUserPolicyHandler()).toBeDefined();
+  });
+
+  describe('CreateUserPolicyHandler.handle', () => {
+    let user;
+    let abilityFactory: CaslAbilityFactory;
+    let handler: CreateUserPolicyHandler;
+
+    beforeEach(() => {
+      user = {
+        id: 'id',
+        userName: 'username',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        role: Roles.Administrator,
+      };
+      abilityFactory = new CaslAbilityFactory();
+      handler = new CreateUserPolicyHandler();
+    });
+
+    it('should return true for administrator', () => {
+      const ability = abilityFactory.createForUser(user);
+      expect(handler.handle(ability)).toEqual(true);
+    });
+
+    it('should return true for manager', () => {
+      user.role = Roles.Manager;
+      const ability = abilityFactory.createForUser(user);
+      expect(handler.handle(ability)).toEqual(true);
+    });
+
+    it('should return false for author', () => {
+      user.role = Roles.Author;
+      const ability = abilityFactory.createForUser(user);
+      expect(handler.handle(ability)).toEqual(false);
+    });
+
+    it('should return false for viewer', () => {
+      user.role = Roles.Viewer;
+      const ability = abilityFactory.createForUser(user);
+      expect(handler.handle(ability)).toEqual(false);
+    });
   });
 });
